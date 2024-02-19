033227062978e3b3f62dd6fb0f20032945a9d818
Calvin Howarth <89119472+calvinhowarth@users.noreply.github.com>
2024-02-19 13:50:57 -0800

    equation display

---
+++ Assets/Scripts/EquationUi.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EquationUi : MonoBehaviour
+{
+    public GameObject equationByte;
+
+    private List<GameObject> symbols;
+    public string equation;
+
+    private int startPosition = 360;
+
+    private void Start()
+    {
+        
+    }
+
+    private void NextTextBox(string symbol)
+    {
+
+    }
+}

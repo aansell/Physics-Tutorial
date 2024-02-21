ac45e70784b11830863d70ad1e514ba7fd540496
Calvin Howarth <89119472+calvinhowarth@users.noreply.github.com>
2024-02-21 12:27:19 -0800

    fix

--- Assets/Scripts/EquationUi.cs
+++ Assets/Scripts/EquationUi.cs
@@ -5,20 +5,5 @@ using TMPro;
 
 public class EquationUi : MonoBehaviour
 {
-    public GameObject equationByte;
 
-    private List<GameObject> symbols;
-    public string equation;
-
-    private int startPosition = 360;
-
-    private void Start()
-    {
-        
-    }
-
-    private void NextTextBox(string symbol)
-    {
-
-    }
 }

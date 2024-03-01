fe253aec954f6714e023c710700ac9397367238f
JackThomas1423 <144173595+JackThomas1423@users.noreply.github.com>
2024-03-01 14:02:09 -0800

    working on moving buttons into div class

--- jack/button.js
+++ jack/button.js
@@ -1,3 +1,5 @@
-function displayResult() {
-    document.getElementById("header").innerHTML = "Have a nice day!";
-}
+document.querySelectorAll(".selector-button").forEach(function(button) {
+    let nextButton = button.querySelector(".selector-button-next");
+    let prevButton = button.querySelector(".selector-button-prev");
+    //add prev button in css <- TODO
+});

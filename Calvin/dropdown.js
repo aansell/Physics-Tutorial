2ebaa845e63263575efe490832e283f9ca023275
Calvin Howarth <89119472+calvinhowarth@users.noreply.github.com>
2024-03-04 13:50:00 -0800

    dropdown
    clean up

--- Calvin/dropdown.js
+++ Calvin/dropdown.js
@@ -1,44 +1,41 @@
+function openDropdown(dropdown) {
+  dropdown.querySelector(".dropdown-list").style.display = "flex";
+  dropdown.querySelector(".dropdown-arrow").textContent = "▲";
+}
+
+function closeDropdown(dropdown) {
+  dropdown.querySelector(".dropdown-list").style.display = "none";
+  dropdown.querySelector(".dropdown-arrow").textContent = "▼";
+}
+
 // closes all dropdowns when the mouse is clicked on anything that is not a dropdown
 document.addEventListener("click", function (event) {
   if (!event.target.closest(".dropdown")) {
     document.querySelectorAll(".dropdown").forEach(function (dropdown) {
-      dropdown.querySelector(".dropdown-list").style.display = "none";
-      dropdown.querySelector(".dropdown-arrow").textContent = "▼";
+      closeDropdown(dropdown);
     });
   }
 });
 
 // controls each dropdown
-let dropdowns = document.querySelectorAll(".dropdown");
-dropdowns.forEach(function (dropdown) {
-  let button = dropdown.querySelector(".dropdown-button");
-  let buttonText = dropdown.querySelector(".dropdown-button-text");
-  let arrow = dropdown.querySelector(".dropdown-arrow");
-  let list = dropdown.querySelector(".dropdown-list");
-  let values = list.querySelectorAll(".dropdown-value");
+document.querySelectorAll(".dropdown").forEach(function (dropdown) {
+  const list = dropdown.querySelector(".dropdown-list");
+  const buttonText = dropdown.querySelector(".dropdown-button-text");
 
-  function open() {
-    arrow.textContent = "▲";
-    list.style.display = "flex";
-  }
+  dropdown
+    .querySelector(".dropdown-button")
+    .addEventListener("click", function () {
+      if (list.style.display === "flex") {
+        closeDropdown(dropdown);
+      } else {
+        openDropdown(dropdown);
+      }
+    });
 
-  function close() {
-    arrow.textContent = "▼";
-    list.style.display = "none";
-  }
-
-  button.addEventListener("click", function () {
-    if (list.style.display === "flex") {
-      close();
-    } else {
-      open();
-    }
-  });
-
-  values.forEach(function (value) {
+  list.querySelectorAll(".dropdown-value").forEach(function (value) {
     value.addEventListener("click", function () {
       buttonText.textContent = value.textContent;
-      close();
+      closeDropdown(dropdown);
     });
   });
 });

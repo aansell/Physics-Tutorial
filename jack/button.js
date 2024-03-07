0c646eabb9b2f56a09c5b345b8d92f9e751e957c
JackThomas1423 <144173595+JackThomas1423@users.noreply.github.com>
2024-03-07 13:02:42 -0800

    fully added blue buttons
    needs better css

--- jack/button.js
+++ jack/button.js
@@ -1,5 +1,14 @@
 document.querySelectorAll(".selector-button").forEach(function(button) {
     let nextButton = button.querySelector(".selector-button-next");
     let prevButton = button.querySelector(".selector-button-prev");
-    //add prev button in css <- TODO
+    nextButton.addEventListener("click", nextButtonFunction());
+    prevButton.addEventListener("click", prevButtonFunction());
 });
+
+function nextButtonFunction() {
+    console.log("next button pressed");
+}
+
+function prevButtonFunction() {
+    console.log("prev button pressed");
+}

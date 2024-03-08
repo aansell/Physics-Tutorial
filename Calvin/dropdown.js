6404f521b2d08e6d48b819bbed4093c6c4befbd6
Calvin Howarth <89119472+calvinhowarth@users.noreply.github.com>
2024-03-08 13:41:55 -0800

    changed behavior

--- Calvin/dropdown.js
+++ Calvin/dropdown.js
@@ -1,3 +1,5 @@
+const dropdowns = document.querySelectorAll(".dropdown");
+
 function openDropdown(dropdown) {
   dropdown.querySelector(".dropdown-list").style.display = "flex";
   dropdown.querySelector(".dropdown-arrow").textContent = "▲";
@@ -8,29 +10,32 @@ function closeDropdown(dropdown) {
   dropdown.querySelector(".dropdown-arrow").textContent = "▼";
 }
 
+function closeAllDropdowns() {
+  dropdowns.forEach(closeDropdown);
+}
+
 // closes all dropdowns when the mouse is clicked on anything that is not a dropdown
 document.addEventListener("click", (event) => {
   if (!event.target.closest(".dropdown")) {
-    document.querySelectorAll(".dropdown").forEach((dropdown) => {
-      closeDropdown(dropdown);
-    });
+    closeAllDropdowns();
   }
 });
 
 // controls each dropdown
-document.querySelectorAll(".dropdown").forEach((dropdown) => {
+dropdowns.forEach((dropdown) => {
   const list = dropdown.querySelector(".dropdown-list");
   const buttonText = dropdown.querySelector(".dropdown-button-text");
 
   dropdown.querySelector(".dropdown-button").addEventListener("click", () => {
-    if (list.style.display === "none") {
-      openDropdown(dropdown);
-    } else {
+    if (list.style.display === "flex") {
       closeDropdown(dropdown);
+    } else {
+      closeAllDropdowns();
+      openDropdown(dropdown);
     }
   });
 
-  list.querySelectorAll(".dropdown-value").forEach((value) => {
+  list.querySelectorAll(".dropdown-option").forEach((value) => {
     value.addEventListener("click", () => {
       buttonText.textContent = value.textContent;
       closeDropdown(dropdown);

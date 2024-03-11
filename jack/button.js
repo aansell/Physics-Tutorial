00052d4f4f0184fc9023100cb8181eb7d37a48b3
JackThomas1423 <144173595+JackThomas1423@users.noreply.github.com>
2024-03-11 13:49:31 -0700

    trying to fix button stuff

--- jack/button.js
+++ jack/button.js
@@ -1,14 +1,9 @@
-document.querySelectorAll(".selector-button").forEach(function(button) {
-    let nextButton = button.querySelector(".selector-button-next");
-    let prevButton = button.querySelector(".selector-button-prev");
-    nextButton.addEventListener("click", nextButtonFunction());
-    prevButton.addEventListener("click", prevButtonFunction());
+let buttons = document.querySelectorAll(".selector-button");
+
+function clicked() {
+    console.log("hello");
+}
+
+buttons.forEach(function (button) {
+    button.addEventListener("click", clicked);
 });
-
-function nextButtonFunction() {
-    console.log("next button pressed");
-}
-
-function prevButtonFunction() {
-    console.log("prev button pressed");
-}

979563f6764e15b90534a9013bd1e662ce32055f
Calvin Howarth <89119472+calvinhowarth@users.noreply.github.com>
2024-03-12 13:19:59 -0700

    comments

--- Calvin/dropdown/dropdown.js
+++ Calvin/dropdown/dropdown.js
@@ -1,15 +1,19 @@
+// finds all the dropdowns when the page loads
 const dropdowns = document.querySelectorAll(".dropdown");
 
+// opens the given dropdown
 function openDropdown(dropdown) {
   dropdown.querySelector(".dropdown-list").style.display = "flex";
   dropdown.querySelector(".dropdown-arrow").textContent = "▲";
 }
 
+// closes the given dropdown
 function closeDropdown(dropdown) {
   dropdown.querySelector(".dropdown-list").style.display = "none";
   dropdown.querySelector(".dropdown-arrow").textContent = "▼";
 }
 
+// closes all the dropdowns in the dropdowns list
 function closeAllDropdowns() {
   dropdowns.forEach(closeDropdown);
 }
@@ -21,11 +25,12 @@ document.addEventListener("click", (event) => {
   }
 });
 
-// controls each dropdown
+// adds event listeners to each dropdown
 dropdowns.forEach((dropdown) => {
   const list = dropdown.querySelector(".dropdown-list");
   const buttonText = dropdown.querySelector(".dropdown-button-text");
 
+  // toggles the given dropdown and closes all other dropdowns when opened
   dropdown.querySelector(".dropdown-button").addEventListener("click", () => {
     if (list.style.display === "flex") {
       closeDropdown(dropdown);
@@ -35,6 +40,7 @@ dropdowns.forEach((dropdown) => {
     }
   });
 
+  // changes the text of the dropdown when one of the values is clicked and closes the dropdown
   list.querySelectorAll(".dropdown-option").forEach((value) => {
     value.addEventListener("click", () => {
       buttonText.textContent = value.textContent;

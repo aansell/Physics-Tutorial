2adad1650d725f049ee496136efc0be875ca21da
JackThomas1423 <144173595+JackThomas1423@users.noreply.github.com>
2024-03-13 13:01:53 -0700

    fully added/fixed buttons

--- jack/button.js
+++ jack/button.js
@@ -1,9 +1,22 @@
+//get all buttons on current html page
 let buttons = document.querySelectorAll(".selector-button");
 
-function clicked() {
-    console.log("hello");
-}
-
+//add event listener for each button
 buttons.forEach(function (button) {
-    button.addEventListener("click", clicked);
+    button.addEventListener("click", (event) => {
+        //maps button id in switch case for each id of button type
+        let buttonId = event.target.id;
+        switch(buttonId) {
+            case 'Continue':
+            console.log("clicked continue");
+            break;
+            case 'Previous':
+            console.log("clicked previous");
+            break;
+            default:
+            //if id is not able to be mapped alert user to an error
+            alert("Error: unknown id tag given " + buttonId);
+            break;
+        }
+    });
 });

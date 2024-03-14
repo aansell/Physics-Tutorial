94e9d45db34f511294043c923b851aa9b85ff540
Calvin Howarth <89119472+calvinhowarth@users.noreply.github.com>
2024-03-14 12:33:08 -0700

    html

---
+++ Calvin/convert/convert.js
@@ -0,0 +1,32 @@
+function convert(value, valueUnit, wantUnit) {
+  return (value / wantUnit) * valueUnit;
+}
+
+const unit = {
+  // time
+  hr: 3600,
+  min: 60,
+  sec: 1,
+
+  // distance
+  mi: 1609.344,
+  km: 1000,
+  m: 1,
+  ft: 0.3048,
+  in: 0.0254,
+  cm: 0.01,
+  mm: 0.001,
+
+  // mass
+  kg: 1000,
+  lb: 453.59237,
+  oz: 28.349523125,
+  g: 1,
+  mg: 0.001,
+};
+
+console.log(convert(1, unit.mi, unit.ft));
+console.log(convert(1, unit.km, unit.m));
+console.log(convert(180, unit.sec, unit.hr));
+console.log(convert(1, unit.g, unit.kg));
+console.log(convert(1, unit.lb, unit.oz));
--- Calvin/dropdown/dropdown.js
+++ Calvin/dropdown/dropdown.js
@@ -1,31 +1,36 @@
+// finds all the dropdowns when the page loads
 const dropdowns = document.querySelectorAll(".dropdown");
 
+// opens the given dropdown
 function openDropdown(dropdown) {
   dropdown.querySelector(".dropdown-list").style.display = "flex";
   dropdown.querySelector(".dropdown-arrow").textContent = "▲";
 }
 
+// closes the given dropdown
 function closeDropdown(dropdown) {
   dropdown.querySelector(".dropdown-list").style.display = "none";
   dropdown.querySelector(".dropdown-arrow").textContent = "▼";
 }
 
+// closes all the dropdowns in the dropdowns list
 function closeAllDropdowns() {
   dropdowns.forEach(closeDropdown);
 }
 
-// closes all dropdowns when the mouse is clicked on anything that is not a dropdown
+// closes all dropdowns when the mouse is clicked on anything that is not a dropdown-button
 document.addEventListener("click", (event) => {
-  if (!event.target.closest(".dropdown")) {
+  if (!event.target.matches(".dropdown-button")) {
     closeAllDropdowns();
   }
 });
 
-// controls each dropdown
+// adds event listeners to each dropdown
 dropdowns.forEach((dropdown) => {
   const list = dropdown.querySelector(".dropdown-list");
   const buttonText = dropdown.querySelector(".dropdown-button-text");
 
+  // toggles the given dropdown and closes all other dropdowns when opened
   dropdown.querySelector(".dropdown-button").addEventListener("click", () => {
     if (list.style.display === "flex") {
       closeDropdown(dropdown);
@@ -35,10 +40,10 @@ dropdowns.forEach((dropdown) => {
     }
   });
 
+  // changes the text of the dropdown when one of the values is clicked
   list.querySelectorAll(".dropdown-option").forEach((value) => {
     value.addEventListener("click", () => {
       buttonText.textContent = value.textContent;
-      closeDropdown(dropdown);
     });
   });
 });

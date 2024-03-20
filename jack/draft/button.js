208d1f27494aa6403e66dce3df9c76a86d17b0e0
JackThomas1423 <144173595+JackThomas1423@users.noreply.github.com>
2024-03-20 14:41:27 -0700

    added stuff

--- jack/draft/button.js
+++ jack/draft/button.js
@@ -4,15 +4,20 @@ class ButtonDiv {
         this.buttonDiv.className = "ButtonDiv";
         var prev = document.createElement("button");
         prev.innerHTML = "Back";
+        prev.style.textAlign = "left";
         prev.addEventListener("click", (event) => {
 
         });
         var next = document.createElement("button");
         next.innerHTML = "Continue";
+        next.style.textAlign = "right";
+        next.addEventListener("click", (event) => {
+
+        });
         this.buttonDiv.appendChild(prev);
         this.buttonDiv.appendChild(next);
         parent.appendChild(this.buttonDiv);
     }
 }
 
-new ButtonDiv(document.getElementById("side-panel"),"prev.hml", "next.html");
+new ButtonDiv(document.getElementById("buttonDiv"),"prev.hml", "next.html");

28c417629c229455a66f7f80178782a9fc12ef55
Calvin Howarth <89119472+calvinhowarth@users.noreply.github.com>
2024-04-03 13:46:43 -0700

    added speed convert

--- Calvin/convert/convert.js
+++ Calvin/convert/convert.js
@@ -1,30 +1,78 @@
 function convert(value, valueUnit, wantUnit) {
-  return (value / wantUnit) * valueUnit;
+  return (value * valueUnit) / wantUnit;
+}
+
+function convertFormat(leftSide) {
+  let leftUnit = null;
+  let rightUnit = null;
+  if (unit[unitSelect.value].hasOwnProperty("top")) {
+    const leftTop =
+      unit[unit[unitSelect.value].top][leftSideUnit.value.split("/")[0]];
+    const leftBottom =
+      unit[unit[unitSelect.value].bottom][leftSideUnit.value.split("/")[1]];
+
+    const rightTop =
+      unit[unit[unitSelect.value].top][rightSideUnit.value.split("/")[0]];
+    const rightBottom =
+      unit[unit[unitSelect.value].bottom][rightSideUnit.value.split("/")[1]];
+
+    leftUnit = leftTop / leftBottom;
+    rightUnit = rightTop / rightBottom;
+  } else {
+    leftUnit = unit[unitSelect.value][leftSideUnit.value];
+    rightUnit = unit[unitSelect.value][rightSideUnit.value];
+  }
+  if (leftSide) {
+    leftSideNumber.value = convert(
+      parseFloat(rightSideNumber.value),
+      rightUnit,
+      leftUnit
+    );
+    if (leftSideNumber.value == "NaN") {
+      leftSideNumber.value = "";
+    }
+  } else {
+    rightSideNumber.value = convert(
+      parseFloat(leftSideNumber.value),
+      leftUnit,
+      rightUnit
+    );
+    if (rightSideNumber.value == "NaN") {
+      rightSideNumber.value = "";
+    }
+  }
 }
 
 const unit = {
-  // time
-  hr: 3600,
-  min: 60,
-  sec: 1,
-
-  // distance
-  mi: 1609.344,
-  km: 1000,
-  m: 1,
-  ft: 0.3048,
-  in: 0.0254,
-  cm: 0.01,
-  mm: 0.001,
-
-  // mass
-  ton: 907184.74,
-  kg: 1000,
-  lb: 453.59237,
-  oz: 28.349523125,
-  g: 1,
-  mg: 0.001,
+  time: {
+    hour: 3600,
+    minute: 60,
+    second: 1,
+  },
+  distance: {
+    mile: 1609.344,
+    kilometer: 1000,
+    meter: 1,
+    feet: 0.3048,
+    inch: 0.0254,
+    centimeter: 0.01,
+    millimeter: 0.001,
+  },
+  mass: {
+    ton: 907184.74,
+    kilogram: 1000,
+    pound: 453.59237,
+    ounce: 28.349523125,
+    gram: 1,
+    milligram: 0.001,
+  },
+  speed: {
+    top: "distance",
+    bottom: "time",
+  },
 };
+
+const unitSelect = document.querySelector(".unit-type");
 
 const equation = document.querySelector(".convert").querySelector(".equation");
 
@@ -37,44 +85,61 @@ const rightSideNumber = rightSide.querySelector(".number-input");
 const rightSideUnit = rightSide.querySelector(".right-side-unit");
 
 for (let key in unit) {
-  leftSideUnit.insertAdjacentHTML(
-    "beforeend",
-    `<option value="${key}">${key}</option>`
-  );
-  rightSideUnit.insertAdjacentHTML(
+  unitSelect.insertAdjacentHTML(
     "beforeend",
     `<option value="${key}">${key}</option>`
   );
 }
 
-leftSideNumber.addEventListener("input", (event) => {
-  rightSideNumber.value = convert(
-    parseFloat(event.target.value),
-    unit[leftSideUnit.value],
-    unit[rightSideUnit.value]
-  );
+function setUnitSelect(unitSelected) {
+  leftSideUnit.innerHTML = "";
+  rightSideUnit.innerHTML = "";
+  if (unitSelected.hasOwnProperty("top")) {
+    for (let top in unit[unitSelected.top]) {
+      for (let bottom in unit[unitSelected.bottom]) {
+        leftSideUnit.insertAdjacentHTML(
+          "beforeend",
+          `<option value="${top}/${bottom}">${top} per ${bottom}</option>`
+        );
+        rightSideUnit.insertAdjacentHTML(
+          "beforeend",
+          `<option value="${top}/${bottom}">${top} per ${bottom}</option>`
+        );
+      }
+    }
+  } else {
+    for (let key in unitSelected) {
+      leftSideUnit.insertAdjacentHTML(
+        "beforeend",
+        `<option value="${key}">${key}</option>`
+      );
+      rightSideUnit.insertAdjacentHTML(
+        "beforeend",
+        `<option value="${key}">${key}</option>`
+      );
+    }
+  }
+}
+
+unitSelect.addEventListener("change", () => {
+  setUnitSelect(unit[unitSelect.value]);
+  convertFormat(false);
 });
 
-leftSideUnit.addEventListener("change", (event) => {
-  rightSideNumber.value = convert(
-    parseFloat(leftSideNumber.value),
-    unit[leftSideUnit.value],
-    unit[rightSideUnit.value]
-  );
+leftSideNumber.addEventListener("input", () => {
+  convertFormat(false);
 });
 
-rightSideNumber.addEventListener("input", (event) => {
-  leftSideNumber.value = convert(
-    parseFloat(event.target.value),
-    unit[rightSideUnit.value],
-    unit[leftSideUnit.value]
-  );
+leftSideUnit.addEventListener("change", () => {
+  convertFormat(false);
 });
 
-rightSideUnit.addEventListener("change", (event) => {
-  leftSideNumber.value = convert(
-    parseFloat(rightSideNumber.value),
-    unit[rightSideUnit.value],
-    unit[leftSideUnit.value]
-  );
+rightSideNumber.addEventListener("input", () => {
+  convertFormat(true);
 });
+
+rightSideUnit.addEventListener("change", () => {
+  convertFormat(false);
+});
+
+setUnitSelect(unit.time);

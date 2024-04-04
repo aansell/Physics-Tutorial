86b3faecfdcf2582a33977dcfb36259e0127b52e
JackThomas1423 <144173595+JackThomas1423@users.noreply.github.com>
2024-04-04 13:54:01 -0700

    got buttons on alternate sides
    need to get div down to the bottom of page

--- jack/draft/button.js
+++ jack/draft/button.js
@@ -1,23 +1,14 @@
 class ButtonDiv {
     constructor(parent,previousPage,NextPage) {
-        this.buttonDivRight = document.createElement("div");
-        this.buttonDivLeft = document.createElement("div");
-
-        this.buttonDivRight.className = "ButtonDiv";
-        this.buttonDivLeft.className = "ButtonDiv";
-
+        var secondDiv = document.createElement("div");
+        //secondDiv.className = "";
         var buttonLeft = document.createElement("button");
         buttonLeft.innerText = "Previous";
         var buttonRight = document.createElement("button");
         buttonRight.innerText = "Continue";
-
-        this.buttonDivLeft.appendChild(buttonLeft);
-        this.buttonDivRight.appendChild(buttonRight);
-
-
-
-        parent.appendChild(this.buttonDivRight);
-        parent.appendChild(this.buttonDivLeft);
+        secondDiv.appendChild(buttonLeft);
+        secondDiv.appendChild(buttonRight);
+        parent.appendChild(secondDiv);
     }
 }
 

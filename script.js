667a4849f0f24234641632f18e54d6baef24170d
lsahler7776 <lsahler7776@student.pps.net>
2024-04-17 09:55:33 -0700

    e

--- script.js
+++ script.js
@@ -42,6 +42,33 @@ class equationJSON {
   }
 }
 
+class problemsJSON{
+  problems;
+
+  constructor() {
+    var jsonProblems = fetch("problems.json").then(response => response.json());
+    this.problems = jsonProblems.then((result) => {
+      var problems = new Array();
+      result.forEach(item => {
+        problems.push(new Problem(item));
+      })
+      return problems;
+    });
+  }
+
+    async size() {
+      return this.problems.then((result) => {
+        return result.length;
+      });
+    }
+    
+    async addToHTML(index, whereToPutIt) {
+      this.problems.then((result) => {
+        ProblemToHTML(result[index], whereToPutIt);
+      });
+    }
+}
+
 class dropBoxDiv {
   constructor(parent, headerText) {
     this.variable = document.createElement("div");
